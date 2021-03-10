a65e31b25e9d75c17e9ba40a7b04e4c967618920
Galahad3x <joelaumedes@gmail.com>
2021-03-10 12:44:49 +0100

    very nice update

--- todo/webtodo/models.py
+++ todo/webtodo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+	text = models.CharField(max_length=100)
+	created = models.DateTimeField()
+	urgency = models.IntegerField(default=1)
+
+
--- todo/webtodo/views.py
+++ todo/webtodo/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 # Req: The HTTP Request
+from .models import Task
+
+
 def main_url(req):
-	return HttpResponse("<h1>Ola k ase</h1>")
+	tsks = Task.objects.order_by('-urgency')
+
+	ctx = {"tasks": tsks}
+	return render(req, "webtodo/tasklist.html", ctx)
+
+# tasks_names = [t.text for t in tsks]
+# text = ",".join(tasks_names)
+# return HttpResponse("<h2>Ola k ase</h2>\n<p>"+text+"</p>")
